96a8d1ce403d294c87366583c935e4f6e654316e
UUMac <36143475+Hua0007@users.noreply.github.com>
2018-02-05 19:30:50 +0800

    hua
    add

--- Helloworld/TestView/TestView.m
+++ Helloworld/TestView/TestView.m
@@ -18,13 +18,15 @@
 }
 
 - (void)addBtns{
-    UIButton *aBtn = [[UIButton alloc] initWithFrame:CGRectMake(10, 30, 50, 40)];
+    UIButton *aBtn = [[UIButton alloc] initWithFrame:CGRectMake(50, 100, 100, 50)];
     [aBtn setTitle:@"Red" forState:UIControlStateNormal];
+    [aBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [aBtn addTarget:self action:@selector(changeBackgroundRed) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:aBtn];
     
-    UIButton *bBtn = [[UIButton alloc] initWithFrame:CGRectMake(10*3+50, 30, 50, 50)];
+    UIButton *bBtn = [[UIButton alloc] initWithFrame:CGRectMake(50, 200, 100, 50)];
     [bBtn setTitle:@"Normal" forState:UIControlStateNormal];
+    [bBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [bBtn addTarget:self action:@selector(changeBackgroundNormal) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:bBtn];
 }
